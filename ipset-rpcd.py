bcbed379d0c10811d55c6176748eb5f5c8a3a524
Felix Eckhofer <felix@eckhofer.com>
2016-05-30 12:29:44 +0200

    Implement command line arguments

--- ipset-rpcd.py
+++ ipset-rpcd.py
@@ -1,6 +1,7 @@
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 import subprocess
 import logging
+import argparse
 
 EXIT_OK = ["OK"]  # We need to always return an array
 
@@ -32,11 +33,27 @@ def stop(user, mac, ip, role, timeout):
         ])
     return EXIT_OK if ret == 0 else ["Error"]
 
-logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
-    logging.info("Starting JSON-RPC daemon...")
-    server = SimpleJSONRPCServer(('localhost', 9090))
+    # Setup logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Parse commandline
+    parser = argparse.ArgumentParser(
+        description='JSON-RPC daemon',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--bind', default='127.0.0.1',
+                        help='set the ip address to bind to')
+    parser.add_argument('--port', type=int, default='9090',
+                        help='set the port to listen on')
+    args = parser.parse_args()
+
+    # Start server
+    logging.info("Starting JSON-RPC daemon at {bind}:{port}...".format(
+        bind=args.bind,
+        port=args.port,
+    ))
+    server = SimpleJSONRPCServer((args.bind, args.port))
     server.register_function(start, 'Start')
     server.register_function(start, 'Update')
     server.register_function(stop, 'Stop')
