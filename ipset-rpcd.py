be1a17f994ecb43c1923743cdada5c2f294ff35e
Felix Eckhofer <felix@eckhofer.com>
2016-06-24 17:31:09 +0200

    Fix syntax error

--- ipset-rpcd.py
+++ ipset-rpcd.py
@@ -51,7 +51,7 @@ def update_ipset(ipset, action, user, mac, ip, role, timeout):
         return False
 
     logging.debug((
-        'User {user}: {action} to/from ipset {ipset}'.format(
+        'User {user}: {action} to/from ipset {ipset}').format(
         user=user, action=action, ipset=ipset))
 
 
